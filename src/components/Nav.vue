c6b68dcbc08aa86382f5fcdd0ef0a432d1f8de5d
yanzewei <shaunyan@sina.com>
2019-03-10 15:44:55 -0300

    UI of nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav>
+        <router-link to="" class="nav-btn">home</router-link>
+        <router-link to="" class="nav-btn">shop list</router-link>
+    </nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
